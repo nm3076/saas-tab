9c9d626997f08941c61e8039249fe76523ebcb5c
kshi18 <karenshi0914@gmail.com>
2021-11-02 04:27:03 -0400

    changing model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,3 @@
 class Link < ApplicationRecord
-    belongs_to :workspace, optional: true
+    belongs_to :workspace, inverse of: :workspace, optional: true
 end
--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -1,3 +1,3 @@
 class Workspace < ApplicationRecord
-    has_many :links
+    has_many :links, inverse of: :links
 end
