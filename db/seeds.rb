53ed1cadff812dea5bd31020748d0f7bb87680de
kshi18 <karenshi0914@gmail.com>
2021-11-02 04:10:27 -0400

    fixing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Workspace.destroy_all
+Link.destroy_all
 
 workspaces = [{ :workspace_name => 'SaaS' }, { :workspace_name => 'MBC' }]
 
@@ -12,8 +14,12 @@ workspaces.each do |workspace|
     Workspace.create!(workspace)
 end
 
-links = [{ :workspace_name => 'SaaS', :link => 'google.com' }, { :workspace_name => 'MBC', :link => 'meta.com' }, { :workspace_name => 'MBC', :link => 'facebook.com' }]
-
-links.each do |l|
-    Link.create!(l)
+workspaces.each_with_index do |obj, index|
+    Link.create!([{:workspace_name => 'class #{index}', :link => 'google.com', :workspace_id => obj.id }])
 end
+
+#    links = [{ :workspace_name => 'SaaS', :link => 'google.com' }, { :workspace_name => 'MBC', :link => 'meta.com' }, { :workspace_name => 'MBC', :link => 'facebook.com' }]
+
+#links.each do |l|
+#    Link.create!(l)
+#end
