d9c79f772ffe60eceb2b01d559a2ebf1d6575c86
kshi18 <karenshi0914@gmail.com>
2021-11-02 11:05:24 -0400

    added to controller, created view for new

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -2,4 +2,18 @@ class WorkspacesController < ApplicationController
     def index
         @workspaces = Workspace.all
     end
+
+    def new
+        # default: render 'new' template
+    end
+
+    def create
+        @workspace = Workspace.create!(workspace_params)
+        flash[:notice] = "#{@workspace.workspace_name} was successfully created!"
+        redirect_to workspaces_path
+    end
+
+    def workspace_params
+        params.require(:workspace).permit(:workspace_name)
+    end
 end
