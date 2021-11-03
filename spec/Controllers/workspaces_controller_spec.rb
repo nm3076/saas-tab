5d94165a9eabf57d2daf7848b45176f34a6b537a
Richard Lopez <richardlopez@dyn-160-39-11-138.dyn.columbia.edu>
2021-11-03 03:47:18 -0400

    Check to see if workspaces has been successfully added

--- spec/Controllers/workspaces_controller_spec.rb
+++ spec/Controllers/workspaces_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe WorkspacesController, type: :controller do
+
+    # test get index
     describe "GET #index" do
       it "returns a successful response" do
         get :index
@@ -14,17 +16,40 @@ RSpec.describe WorkspacesController, type: :controller do
 
     end
 
+
+    #test get new view
     describe "GET #new" do
       it "returns a successful response" do
-        get :index
+        get :new
         expect(response).to be_successful
       end
 
       it "renders the new template" do
-        get :index
+        get :new
         expect(response).to render_template("new")
       end
+    end
 
+
+    # test create workspace
+    describe "create workspace" do
+      it "creates workspace successfully and redirects to workspaces_path" do
+        post :create, params: {:workspace => {:workspace_name => "PLT" }}
+        expect(response).to redirect_to workspaces_path
+        Workspace.find_by(:workspace_name => "PLT").destroy
+      end
+    end
+
+    describe "create workspace" do
+      it "creates workspace successfully and redirects to workspaces_path" do
+        post :create, params: {:workspace => {:workspace_name => "PLT" }}
+        @all_workspaces = Workspace.all
+        @made_workspace = Workspace.find_by(:workspace_name => "PLT")
+
+        expect(@all_workspaces).to include(@made_workspace)
+        
+        Workspace.find_by(:workspace_name => "PLT").destroy
+      end
     end
 
 end
