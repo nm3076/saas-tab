c952d7df130bed168766ab8d5f464d106202032b
Jessica <jp3864@columbia.edu>
2021-11-03 03:19:16 -0400

    added for new

--- spec/Controllers/workspaces_controller_spec.rb
+++ spec/Controllers/workspaces_controller_spec.rb
@@ -7,14 +7,24 @@ RSpec.describe WorkspacesController, type: :controller do
         expect(response).to be_successful
       end
 
-      it "assigns @workspaces" do
-        
-      end
-
       it "renders the index template" do
         get :index
         expect(response).to render_template("index")
       end
 
     end
+
+    describe "GET #new" do
+      it "returns a successful response" do
+        get :index
+        expect(response).to be_successful
+      end
+
+      it "renders the new template" do
+        get :index
+        expect(response).to render_template("new")
+      end
+
+    end
+
 end
