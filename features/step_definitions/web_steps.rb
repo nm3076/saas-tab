18785cd5970519783a4aec6a0c85a54bb38c55bf
kshi18 <karenshi0914@gmail.com>
2021-11-03 15:13:56 -0400

    cucumber coverage now at 72

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -76,31 +76,31 @@ end
 # TODO: Add support for checkbox, select or option
 # based on naming conventions.
 #
-When /^(?:|I )fill in the following:$/ do |fields|
-  fields.rows_hash.each do |name, value|
-    When %{I fill in "#{name}" with "#{value}"}
-  end
-end
+# When /^(?:|I )fill in the following:$/ do |fields|
+#   fields.rows_hash.each do |name, value|
+#     When %{I fill in "#{name}" with "#{value}"}
+#   end
+# end
 
-When /^(?:|I )select "([^"]*)" from "([^"]*)"$/ do |value, field|
-  select(value, :from => field)
-end
+# When /^(?:|I )select "([^"]*)" from "([^"]*)"$/ do |value, field|
+#   select(value, :from => field)
+# end
 
-When /^(?:|I )check "([^"]*)"$/ do |field|
-  check(field)
-end
+# When /^(?:|I )check "([^"]*)"$/ do |field|
+#   check(field)
+# end
 
-When /^(?:|I )uncheck "([^"]*)"$/ do |field|
-  uncheck(field)
-end
+# When /^(?:|I )uncheck "([^"]*)"$/ do |field|
+#   uncheck(field)
+# end
 
-When /^(?:|I )choose "([^"]*)"$/ do |field|
-  choose(field)
-end
+# When /^(?:|I )choose "([^"]*)"$/ do |field|
+#   choose(field)
+# end
 
-When /^(?:|I )attach the file "([^"]*)" to "([^"]*)"$/ do |path, field|
-  attach_file(field, File.expand_path(path))
-end
+# When /^(?:|I )attach the file "([^"]*)" to "([^"]*)"$/ do |path, field|
+#   attach_file(field, File.expand_path(path))
+# end
 
 Then /^(?:|I )should see "([^"]*)"$/ do |text|
   if page.respond_to? :should
