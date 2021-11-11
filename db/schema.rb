2dfc2707cc6e010f1c64a1dc0c73cf7db4fd799b
kshi18 <karenshi0914@gmail.com>
2021-11-10 22:39:59 -0500

    created post link from registration page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_031605) do
+ActiveRecord::Schema.define(version: 2021_11_11_023218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_11_10_031605) do
     t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
   end
 
   create_table "workspaces", force: :cascade do |t|
