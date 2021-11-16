357a19678af9bfea341c4c2caf1509c16af65044
Richard Lopez <richardlopez@dyn-160-39-11-138.dyn.columbia.edu>
2021-11-16 03:12:52 -0500

    fixed minor bug to validate username, not name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
     has_many :workspaces
     # to add more validation, make validation restrictions here for user
     before_save { self.email = email.downcase }
-    validates :name, presence: true
+    validates :username, presence: true
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-                    
+
     has_secure_password
 
 
