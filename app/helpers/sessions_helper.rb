c17d1660875c2f61f70b66e53130708c389ba2f7
Richard Lopez <richardlopez@dyn-160-39-11-138.dyn.columbia.edu>
2021-11-16 03:05:30 -0500

    added testing for sign in with valid info, but is not working bc no redirect. Also added logout functionality. Logout not tested bc login is broken

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,14 +1,22 @@
 module SessionsHelper
+    #store the user id of the logged in user within session hash
     def log_in(user)
         session[:user_id] = user.id
     end
 
+    #returns the current logged in user
     def current_user
         @current_user ||= User.find_by(id: session[:user_id])
     end
 
+    #returns true if the user is logged in
     def logged_in?
         !current_user.nil?
+    end
+
+    def log_out
+        session.delete(:user_id)
+        @current_user = nil
     end
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,20 @@
 class User < ApplicationRecord
     has_many :workspaces
     # to add more validation, make validation restrictions here for user
+    before_save { self.email = email.downcase }
+    validates :name, presence: true
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+                    
     has_secure_password
+
+
+    #used by user features during testing
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                      BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
 end
