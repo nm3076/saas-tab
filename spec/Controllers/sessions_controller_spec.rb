7482258632800975a1b85c1156e54976a51d6b8b
Richard Lopez <richardlopez@dyn-160-39-11-138.dyn.columbia.edu>
2021-11-17 11:43:37 -0500

    continuin testing in butcaf soon

--- spec/Controllers/sessions_controller_spec.rb
+++ spec/Controllers/sessions_controller_spec.rb
@@ -4,13 +4,33 @@ RSpec.describe SessionsController, type: :controller do
 
     # test get new session
     describe "GET new session" do
-      it "returns a successful response" do
+      it "returns login page" do
+        post :create, params: { :session => { :email => "l3020@columbia.edu", :password => "foobar" }}
         get :new
-        expect(response).to be_successful
-      end
-      it "renders the new template" do
-        get :new
+        expect(flash[:danger]).to match(/Invalid email or password combination/)
         expect(response).to render_template("new")
+      end 
+      
+      it "returns a successful response" do
+        user = User.create!(:username => "rl3020", :email => "rl3020@columbia.edu", :first_name => "Richard", :last_name => "Lopez", password: "foobar", password_confirmation: "foobar")
+        post :create, params: { :session => { :email => "rl3020@columbia.edu", :password => "foobar" }}
+        get :new
+        expect(response).to redirect_to workspaces_path
+
+        user.destroy
+
+      end
+
+      it "login user to create new session" do
+        user = User.create!(:username => "rl3020", :email => "rl3020@columbia.edu", :first_name => "Richard", :last_name => "Lopez", password: "foobar", password_confirmation: "foobar")
+        get :new
+        post :create, params: { :session => { :email => "rl3020@columbia.edu", :password => "foobar" }}
+        puts "response from create path"
+        puts response.code
+        expect(response).to be_successful
+
+        user.destroy
+
       end
       
     end
