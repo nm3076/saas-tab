352d262ce2bd159b93932fa9867b08ff435a8d25
Jessica <jp3864@columbia.edu>
2021-11-17 10:41:51 -0500

    74 LOC

--- spec/Controllers/users_controller_spec.rb
+++ spec/Controllers/users_controller_spec.rb
@@ -17,14 +17,15 @@ RSpec.describe UsersController, type: :controller do
     # test create new user
     describe "create new user" do
         it "succesfully creates a new user" do
-            post :create, params: {:user => {:username => "mikepeng", :email => "michaelpeng@gmail.com", :first_name => "Michael",
-            :last_name => "Peng", :password => "ucla", :password_confirmation => "ucla"}}
-            @created_user = User.find_by(:username => "mikepeng")
+            post :create, params: {:user => {:username => "cindyy", :email => "cindyguo@gmail.com", :first_name => "Cindy",
+            :last_name => "Guo", :password => "ucla", :password_confirmation => "ucla"}}
+            @created_user = User.find_by(:username => "cindyy")
             user_id = @created_user.id
             expect(response).to be_successful
-          
+            expect(response).to redirect_to workspaces_path
+            expect(flash[:success]).to match(/Welcome to the tab!/)
+            User.find_by(:username  => "cindyy").destroy
         end 
-
     end
 
     # test show specific user
@@ -35,8 +36,7 @@ RSpec.describe UsersController, type: :controller do
         user_id = User.find_by(:username => "mikepeng").id
         get :show, params: {:id => user_id}
         expect(response).to be_successful
-        expect(flash[:success]).to match(/Welcome to the tab!/)
-        expect(response).to redirect_to user_path(user_id)
+        
         User.find_by(:username  => "mikepeng").destroy
       end
     
@@ -50,9 +50,6 @@ RSpec.describe UsersController, type: :controller do
       end
     end
 
-    
-    
-
     # edit a user
     describe "edit a user" do
         it "successfuly edits a user" do
@@ -61,6 +58,7 @@ RSpec.describe UsersController, type: :controller do
             user_id = User.find_by(:username => "mikepeng").id
             get :edit, params: {:id => user_id}
             expect(response).to be_successful
+            User.find_by(:username  => "mikepeng").destroy
         end
     end
 
