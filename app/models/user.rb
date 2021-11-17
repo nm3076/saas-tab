173443259884d7dd75d95da8014d285d60a30695
Richard Lopez <richardlopez@dyn-160-39-11-138.dyn.columbia.edu>
2021-11-17 12:34:53 -0500

    finished failed signup for user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,9 @@ class User < ApplicationRecord
 
 
     #used by user features during testing
-    def User.digest(string)
-        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                      BCrypt::Engine.cost
-        BCrypt::Password.create(string, cost: cost)
-    end
+    # def User.digest(string)
+    #     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+    #                                                   BCrypt::Engine.cost
+    #     BCrypt::Password.create(string, cost: cost)
+    # end
 end
