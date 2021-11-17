9a78f4d2dbac780642aae702b410a5a538689d75
Richard Lopez <richardlopez@dyn-160-39-11-138.dyn.columbia.edu>
2021-11-17 10:44:24 -0500

    changed route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
       if @user.save
         log_in @user
         flash[:success] = "Welcome to the tab!"
-        redirect_to @user
+        redirect_to workspaces_path
       else
         render 'new'
       end
